2dd84705417c5d9521b3ebc3661398fdfe4c3474
MiloCo86 <cpaez@pursuit.org>
2024-05-17 15:13:27 -0400

    Routes completes

--- src/components/pets/PetsList.jsx
+++ src/components/pets/PetsList.jsx
@@ -1,9 +1,11 @@
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 import React from "react";
 import PetsListNav from "./PetsListNav";
 import Pet from "./Pet";
 import "./PetsList.css";
 
 export const PetsList = ({ pets }) => {
+  
   const [cats, dogs] = pets.reduce(
     (acc, pet) => {
       const position = pet.kind === "Cat" ? 0 : 1;
@@ -17,14 +19,17 @@ export const PetsList = ({ pets }) => {
     <section className="pets-wrapper">
       <PetsListNav cats={cats} dogs={dogs} />
       <section className="pets-list">
+
+        
         {/* All cats section */}
         {cats.map((cat) => (
-          <Pet key={cat.id} kind="cat" pet={cat} />
+          <Pet key={cat.id} kind={cat.kind.toLowerCase()} pet={cat} />
         ))}
 
         {/* All dogs section */}
+
         {dogs.map((dog) => (
-          <Pet key={dog.id} kind="dog" pet={dog} />
+          <Pet key={dog.id} kind={dog.kind.toLowerCase()} pet={dog} />
         ))}
       </section>
     </section>
