b1bdc3615496295b45704d9d85e86891c10dfb7e
jmoncayo-pursuit <jmoncayo@pursuit.org>
2024-05-18 02:35:08 -0400

    feat: Add 404 Page and Styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
+
 import React from "react";
 import { useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 // Import components
 import Nav from "./components/common/Nav.jsx";
@@ -14,27 +14,34 @@ import { employeeData } from "./data/employees.js";
 import { ownerData } from "./data/owners";
 import { petData } from "./data/pets";
 
+import { Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+import NotFound from "./components/NotFound";
+
 function App() {
   const [employees] = useState(employeeData);
   const [owners] = useState(ownerData);
   const [pets] = useState(petData);
 
   return (
-  <Router>
-    <div className="wrapper">
-      <Nav />
-      <Routes>
-        <Route path="/" element={<Home employees={employees} owners={owners} pets={pets} />} />
-        <Route path="/staff" element={<StaffList employees={employees} />} />
-        <Route path="/pets" element={<PetsList pets={pets} />} />
-        <Route path="/pets/cats" element={<PetsList pets={pets.filter((pet) => pet.kind === "Cat")} />} />
-        <Route path="/pets/dogs" element={<PetsList pets={pets.filter((pet) => pet.kind === "Dog")} />} />
-        <Route path="*" element={<Home employees={employees} owners={owners} pets={pets} />} />
-      </Routes>
-      <Footer />
-    </div>
-  </Router>
-  
+    <BrowserRouter>
+      <div className="wrapper">
+        <Nav />
+        <Routes>
+          <Route
+            path="/"
+            element={<Home employees={employees} owners={owners} pets={pets} />}
+          />
+          <Route path="/staff" element={<StaffList employees={employees} />} />
+          <Route path="/pets" element={<PetsList pets={pets} />} />
+          <Route path="/pets/cats" element={<PetsList pets={pets.filter((pet) => pet.kind === "Cat")} />} />
+          <Route path="/pets/dogs" element={<PetsList pets={pets.filter((pet) => pet.kind === "Dog")} />
+            }/>
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 
